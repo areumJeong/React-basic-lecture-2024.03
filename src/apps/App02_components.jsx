7ee1d0881934d26862675f89eb451a80baffc544
areumJeong <add5833@naver.com>
2024-03-28 11:48:51 +0900

    React

---
+++ src/apps/App02_components.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Card from '../components/Card';
+
+function App() {
+  return (
+    <>    
+      <Card />
+      <br />
+      <Card />
+    </>
+  );
+}
+
+export default App;
