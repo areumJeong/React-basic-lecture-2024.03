7b6cd96bb8e0dd0f1933fce03534b62cfef39ad7
areumJeong <add5833@naver.com>
2024-04-05 14:02:09 +0900

    React MUI

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './apps/App21_router';
+// import App from './apps/App99_exam';
 import reportWebVitals from './reportWebVitals';
+import BasicModal from './mui/BasicModal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{marginTop:'40px', marginLeft: '20px', marginRight: '20px', }}>
+      <BasicModal />
+    </div>
   </React.StrictMode>
 );
 
